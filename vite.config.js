aac90a8584412b351e11bc03e8c456b6f30f5dd1
ShivaRK <shivarkcodes@gmail.com>
2025-03-12 12:15:43 +0530

    delete vercel.json

--- vite.config.js
+++ vite.config.js
@@ -26,7 +26,6 @@ export default defineConfig({
     // Allow a strict Content-Security-Policy
     // withtout inlining assets as base64:
     assetsInlineLimit: 0,
-    sourcemap:false
   },
   ssr: {
     optimizeDeps: {
